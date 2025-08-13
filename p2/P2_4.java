2f88a97a8120866d9070ea79cffc83d41a98a78e
24ftt1824 <24FTT1824@student.pb.edu.bn>
2025-08-13 23:35:23 +0800

    Add files via upload

---
+++ p2/P2_4.java
@@ -0,0 +1,9 @@
+public class P2_4 {
+    public static void main(String[] args) {
+        double result = (7.5 * 2.5 - 1.5 * 3) / (4.0 - 2.5);
+        System.out.println("         7.5 x 2.5 - 1.5 x 3 ");
+        System.out.println("Result: ---------------------  = " + result);
+        System.out.println("               4 - 2.5  ");
+
+    }
+}
---
+++ p2/P2_5.java
@@ -0,0 +1,13 @@
+public class P2_5 {
+    public static void main(String[] args) {
+        System.out.println("       .= \"\" =.                 ");
+        System.out.println("     /          \\                ");
+        System.out.println("    |  0      0  |                ");
+        System.out.println("     \\    /\\    /               ");
+        System.out.println("     / '-=\\/=-' \\               ");
+        System.out.println("    / /        \\ \\   CHIP CHIP! ");
+        System.out.println("    \\/\\        /\\/             ");
+        System.out.println("       '.     .'                  ");
+        System.out.println("        /|\\/|\\                  ");
+    }
+}
